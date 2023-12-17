34e117ca87e7503981d1084bbc3b6f6b3ff42f01
sqshydl <blankjester312@gmail.com>
2023-12-17 18:17:39 +0700

    Backend or server already connect to mysql database

--- server/server.js
+++ server/server.js
@@ -1,9 +1,35 @@
 import express from 'express';
+import mysql from 'mysql2';
 
 const app = express();
 
+// MySQL database connection configuration
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'rpl',
+});
+
+// Check the database connection
+db.connect((err) => {
+  if (err) {
+    console.error('Error connecting to MySQL database:', err);
+  } else {
+    console.log('Connected to MySQL database');
+  }
+});
+
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  // Perform a simple database query
+  db.query('SELECT * FROM pc_profit', (error, results) => {
+    if (error) {
+      console.error('Error executing query:', error);
+      res.status(500).send('Error executing query');
+    } else {
+      res.json(results);
+    }
+  });
 });
 
 app.listen(3001, () => {
